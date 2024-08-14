229d5bc494aa1437f31dd7373372dabeac3324df
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2024-08-14 12:10:29 +0200

    Add test-diff.php

--- test-diff.php
+++ test-diff.php
@@ -9,17 +9,14 @@ require_once 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 $testEngineConfiguration = new EngineConfiguration('stockfish');
 $testEngineConfiguration
     ->addOption('Hash', '256')
-    ->addOption('MultiPV', '100')
+    ->addOption('MultiPV', '2')
     ->addOption('Threads', '1');
 $testEngineConfiguration->setPathToPolyglotRunDir(__DIR__);
 $chessBestMoveService = new ChessBestMove($testEngineConfiguration);
 
-$fen = 'r1b1kbnr/pp1ppppp/1qn5/8/4P3/1N6/PPP2PPP/RNBQKB1R b KQkq - 2 5';
+$fen = '4r1k1/5ppp/p2br1q1/1p1p4/3P4/2P1BPPb/PP1Q3P/R3RNK1 b - - 6 23';
+$move = 'h3f1';
 
-$diff = $chessBestMoveService->getDiff(
-    $fen,
-    'g7g6',
-    100,
-);
+$diff = $chessBestMoveService->getDiff($fen, $move, 100);
 
 var_dump($diff);
